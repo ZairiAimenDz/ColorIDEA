f28d0ce1eb48b63827f3ef784f1f896b60649c11
ZairiAimenDz <zairiaimendev@gmail.com>
2020-12-22 12:57:11 +0100

    Finished MVP

--- ColorIDEA/Program.cs
+++ ColorIDEA/Program.cs
@@ -1,3 +1,4 @@
+using ColorIDEA.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,7 +19,7 @@ namespace ColorIDEA
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddScoped<ClipboardService>();
             await builder.Build().RunAsync();
         }
     }
---
+++ ColorIDEA/Services/ClipboardService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ColorIDEA.Services
+{
+    public class ClipboardService
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public ClipboardService(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public ValueTask<string> ReadTextAsync()
+        {
+            return _jsRuntime.InvokeAsync<string>("navigator.clipboard.readText");
+        }
+
+        public ValueTask WriteTextAsync(string text)
+        {
+            return _jsRuntime.InvokeVoidAsync("navigator.clipboard.writeText", text);
+        }
+    }
+}
